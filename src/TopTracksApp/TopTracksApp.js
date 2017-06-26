4cade2a3a299e0d10e5813bb3bc928a6a6231879
Daniel Jamrozik <djamro2@illinois.edu>
2017-06-26 01:06:42 -0400

    show some data on screen, build framework

--- src/TopTracksApp/TopTracksApp.js
+++ src/TopTracksApp/TopTracksApp.js
@@ -7,7 +7,8 @@ class TopTracksApp extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      showSpinner: true
+      showSpinner: true,
+      selectedTerm: 'short'
     };
   };
 
@@ -29,7 +30,7 @@ class TopTracksApp extends Component {
 
   render() {
     return (
-      <div className="top-tracks-page">
+      <div className="top-items-page">
         {this.state.showSpinner ? this.renderLoadingScreen() : this.renderTopTracksScreen()}
       </div>
     );
@@ -47,12 +48,69 @@ class TopTracksApp extends Component {
   };
 
   renderTopTracksScreen() {
+    const class_short_term = this.state.selectedTerm == 'short' ? "select-term-tab-selected" : "";
+    const class_medium_term = this.state.selectedTerm == 'medium' ? "select-term-tab-selected" : "";
+    const class_long_term = this.state.selectedTerm == 'long' ? "select-term-tab-selected" : "";
+
     return (
-      <div className="top-tracks-screen">
-        Top tracks screen
+      <div className="top-items-screen">
+        <div className="select-term-tabs">
+          <div className={`select-term-tab ${class_short_term}`} onClick={() => this.handleTermChange('short')}>
+            <span>SHORT TERM</span>
+          </div>
+          <div className={`select-term-tab ${class_medium_term}`} onClick={() => this.handleTermChange('medium')}>
+            <span>MEDIUM TERM</span>
+          </div>
+          <div className={`select-term-tab ${class_long_term}`} onClick={() => this.handleTermChange('long')}>
+            <span>LONG TERM</span>
+          </div>
+        </div>
+        <div className="top-items-body">
+          <div className="top-tracks">
+            <p className="header">TOP TRACKS</p>
+            {this.renderTopItems('tracks')}
+          </div>
+          <div className="top-artists">
+            <p className="header">TOP ARTISTS</p>
+            {this.renderTopItems('artists')}
+          </div>
+        </div>
       </div>
     )
   };
+
+  renderTopItems(type) {
+    // only render this part once the data exists
+    if (!this.state || !this.state.topItems) {
+      return null;
+    }
+
+    var term = this.state.selectedTerm;
+    var topItemsCategory = type + this.capitalize(term) + 'Term';
+    var topItemsInCategory = this.state.topItems[topItemsCategory]['items']
+
+    return (
+      <div className="top-items">
+        {topItemsInCategory.map(function(object, i){
+          return (
+            <div className="top-item" key={i}>
+              {object.name}
+            </div>
+          )
+        })}
+      </div>
+    );
+  };
+
+  handleTermChange(newTerm) {
+    this.setState({
+      selectedTerm: newTerm
+    })
+  }
+
+  capitalize(s){
+    return s && s[0].toUpperCase() + s.slice(1);
+  }
 }
 
 export default TopTracksApp;
